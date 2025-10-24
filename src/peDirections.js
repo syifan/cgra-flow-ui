9ec035774047b59912eaf45b8bff4f8c8cc5bf74
Yifan Sun <profyifansun@gmail.com>
2025-10-24 11:35:44 -0400

    Flip north and south

--- src/peDirections.js
+++ src/peDirections.js
@@ -1,14 +1,14 @@
 export const PE_DIRECTIONS = ['n', 'ne', 'e', 'se', 's', 'sw', 'w', 'nw'];
 
 export const PE_DIRECTION_OFFSETS = {
-  n: { dx: 0, dy: -1 },
-  ne: { dx: 1, dy: -1 },
+  n: { dx: 0, dy: 1 },
+  ne: { dx: 1, dy: 1 },
   e: { dx: 1, dy: 0 },
-  se: { dx: 1, dy: 1 },
-  s: { dx: 0, dy: 1 },
-  sw: { dx: -1, dy: 1 },
+  se: { dx: 1, dy: -1 },
+  s: { dx: 0, dy: -1 },
+  sw: { dx: -1, dy: -1 },
   w: { dx: -1, dy: 0 },
-  nw: { dx: -1, dy: -1 }
+  nw: { dx: -1, dy: 1 }
 };
 
 export const PE_OPPOSITE_DIRECTIONS = {
